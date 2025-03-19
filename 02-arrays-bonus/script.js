4569c4f44591559405a51bf44ef417b8566c9dc0
marcopriests <prtmrc98@gmail.com>
2025-03-19 15:35:16 +0100

    [BONUS] 02 - 4 done

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -33,7 +33,9 @@ console.log("Questo è l'array senza 'Ed': " + teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes("Fabio");
+
+console.log("Fabio è incluso nell'array? " + isFabioPresent)
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
